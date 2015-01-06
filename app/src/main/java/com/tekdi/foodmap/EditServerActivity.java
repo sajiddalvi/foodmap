a88fbf93b08e6f35e756017633d561a6b5a5abbf
Sajid Dalvi <sajid.dalvi@gmail.com>
2015-01-06 07:22:53 -0600

    added support for automatically getting the address

--- app/src/main/java/com/tekdi/foodmap/EditServerActivity.java
+++ app/src/main/java/com/tekdi/foodmap/EditServerActivity.java
@@ -1,10 +1,13 @@
 package com.tekdi.foodmap;
 
-import android.app.Activity;
 import android.content.Context;
 import android.location.Address;
 import android.location.Geocoder;
+import android.location.Location;
+import android.os.AsyncTask;
+import android.os.Build;
 import android.os.Bundle;
+import android.support.v4.app.FragmentActivity;
 import android.util.Log;
 import android.util.Pair;
 import android.view.View;
@@ -12,21 +15,37 @@ import android.widget.AdapterView;
 import android.widget.AdapterView.OnItemSelectedListener;
 import android.widget.ArrayAdapter;
 import android.widget.EditText;
+import android.widget.ProgressBar;
 import android.widget.Spinner;
+import android.widget.Toast;
 
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.GooglePlayServicesClient;
+import com.google.android.gms.location.LocationClient;
+import com.google.android.gms.location.LocationListener;
 import com.tekdi.foodmap.backend.serveFoodEntityApi.model.ServeFoodEntity;
 
 import java.io.IOException;
 import java.util.List;
+import java.util.Locale;
 
 /**
  * Created by fsd017 on 12/27/14.
  */
-public class EditServerActivity extends Activity implements OnItemSelectedListener {
+public class EditServerActivity extends FragmentActivity
+        implements OnItemSelectedListener,
+        GooglePlayServicesClient.ConnectionCallbacks,
+        GooglePlayServicesClient.OnConnectionFailedListener,
+        LocationListener {
 
     private String cuisineSelected = "";
     private Long serverId;
     private Boolean addingNewServer = false;
+    private ProgressBar mActivityIndicator;
+    private EditText mAddress;
+    private Location mLocation;
+    private LocationClient mLocationClient;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -39,7 +58,6 @@ public class EditServerActivity extends Activity implements OnItemSelectedListen
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         spinner.setAdapter(adapter);
         spinner.setOnItemSelectedListener(this);
-
 
         if (Prefs.getServeIdPref(this) == "") {
             setTitle("Add Server");
@@ -55,6 +73,118 @@ public class EditServerActivity extends Activity implements OnItemSelectedListen
             serverId = Long.parseLong(Prefs.getServeIdPref(this));
             spinner.setSelection(adapter.getPosition(cuisine));
         }
+
+        mActivityIndicator =
+                (ProgressBar) findViewById(R.id.address_progress);
+        mAddress =(EditText) findViewById(R.id.server_address_text);
+        mLocationClient = new LocationClient(this, this, this);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+    }
+
+
+    /*
+     * Called when the Activity becomes visible.
+     */
+    @Override
+    protected void onStart() {
+        super.onStart();
+        // Connect the client.
+        mLocationClient.connect();
+    }
+
+    /*
+     * Called when the Activity is no longer visible.
+     */
+    @Override
+    protected void onStop() {
+        // Disconnecting the client invalidates it.
+        mLocationClient.disconnect();
+        super.onStop();
+    }
+
+    /*
+* Called by Location Services when the request to connect the
+* client finishes successfully. At this point, you can
+* request the current location or start periodic updates
+*/
+    @Override
+    public void onConnected(Bundle dataBundle) {
+        // Display the connection status
+
+        mLocation = mLocationClient.getLastLocation();
+        getAddress(getWindow().getDecorView().findViewById(android.R.id.content));
+
+        Toast.makeText(this, "Connected " + mLocation.getLatitude() + "," + mLocation.getLongitude(), Toast.LENGTH_SHORT).show();
+    }
+
+    /*
+     * Called by Location Services if the connection to the
+     * location client drops because of an error.
+     */
+    @Override
+    public void onDisconnected() {
+        // Display the connection status
+        Toast.makeText(this, "Disconnected. Please re-connect.",
+                Toast.LENGTH_SHORT).show();
+    }
+
+    /*
+     * Called by Location Services if the attempt to
+     * Location Services fails.
+     */
+    @Override
+    public void onConnectionFailed(ConnectionResult connectionResult) {
+        /*
+         * Google Play services can resolve some errors it detects.
+         * If the error has a resolution, try sending an Intent to
+         * start a Google Play services activity that can resolve
+         * error.
+         */
+        if (connectionResult.hasResolution()) {
+            // Sajid try {
+            // Start an Activity that tries to resolve the error
+
+                /* Sajid - compile error can't resole CONNECTION_FAILURE_RESOLUTION_REQUEST
+                 * connectionResult.startResolutionForResult(
+                 *       this,
+                 *       CONNECTION_FAILURE_RESOLUTION_REQUEST);
+                 */
+
+            Toast.makeText(this, "CONNECTION_FAILURE_RESOLUTION_REQUEST",
+                    Toast.LENGTH_SHORT).show();
+
+                /*
+                 * Thrown if Google Play services canceled the original
+                 * PendingIntent
+                 */
+            // Sajid} catch (IntentSender.SendIntentException e) {
+            // Sajid    // Log the error
+            // Sajid    e.printStackTrace();
+            // Sajid}
+        } else {
+            /*
+             * If no resolution is available, display a dialog to the
+             * user with the error.
+             */
+            //Sajid showErrorDialog(connectionResult.getErrorCode());
+        }
+    }
+
+    // Define the callback method that receives location updates
+    @Override
+    public void onLocationChanged(Location location) {
+        // Report to the UI that the location was updated
+        String msg = "Updated Location: " +
+                Double.toString(location.getLatitude()) + "," +
+                Double.toString(location.getLongitude());
+        Toast.makeText(this, msg, Toast.LENGTH_SHORT).show();
+        mLocation.setLatitude(location.getLatitude());
+        mLocation.setLongitude(location.getLongitude());
+        getAddress(getWindow().getDecorView().findViewById(android.R.id.content));
     }
 
     public void onItemSelected(AdapterView<?> parent, View view,
@@ -124,4 +254,92 @@ public class EditServerActivity extends Activity implements OnItemSelectedListen
         finish();
     }
 
+    public void getAddress(View v) {
+        // Ensure that a Geocoder services is available
+        if (Build.VERSION.SDK_INT >=
+                Build.VERSION_CODES.GINGERBREAD
+                &&
+                Geocoder.isPresent()) {
+            // Show the activity indicator
+            mActivityIndicator.setVisibility(View.VISIBLE);
+            /*
+             * Reverse geocoding is long-running and synchronous.
+             * Run it on a background thread.
+             * Pass the current location to the background task.
+             * When the task finishes,
+             * onPostExecute() displays the address.
+             */
+            (new GetAddressTask(this)).execute(mLocation);
+        }
+    }
+
+    private class GetAddressTask extends
+            AsyncTask<Location, Void, String> {
+        Context mContext;
+        public GetAddressTask(Context context) {
+            super();
+            mContext = context;
+        }
+        @Override
+        protected String doInBackground(Location... params) {
+            Geocoder geocoder =
+                    new Geocoder(mContext, Locale.getDefault());
+            // Get the current location from the input parameter list
+            Location loc = params[0];
+            // Create a list to contain the result address
+            List<Address> addresses = null;
+            try {
+                /*
+                 * Return 1 address.
+                 */
+                addresses = geocoder.getFromLocation(loc.getLatitude(),
+                        loc.getLongitude(), 1);
+            } catch (IOException e1) {
+                Log.e("sajid",
+                        "IO Exception in getFromLocation()");
+                e1.printStackTrace();
+                return ("IO Exception trying to get address");
+            } catch (IllegalArgumentException e2) {
+                // Error message to post in the log
+                String errorString = "Illegal arguments " +
+                        Double.toString(loc.getLatitude()) +
+                        " , " +
+                        Double.toString(loc.getLongitude()) +
+                        " passed to address service";
+                Log.e("sajid", errorString);
+                e2.printStackTrace();
+                return errorString;
+            }
+            // If the reverse geocode returned an address
+            if (addresses != null && addresses.size() > 0) {
+                // Get the first address
+                Address address = addresses.get(0);
+                /*
+                 * Format the first line of address (if available),
+                 * city, and country name.
+                 */
+                String addressText = String.format(
+                        "%s, %s, %s",
+                        // If there's a street address, add it
+                        address.getMaxAddressLineIndex() > 0 ?
+                                address.getAddressLine(0) : "",
+                        // Locality is usually a city
+                        address.getLocality(),
+                        // The country of the address
+                        address.getCountryName());
+                // Return the text
+                return addressText;
+            } else {
+                return "No address found";
+            }
+        }
+
+        @Override
+        protected void onPostExecute(String address) {
+            // Set activity indicator visibility to "gone"
+            mActivityIndicator.setVisibility(View.GONE);
+            // Display the results of the lookup.
+            mAddress.setText(address);
+        }
+    }
 }
